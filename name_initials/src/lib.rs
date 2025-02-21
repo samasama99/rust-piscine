95be0f4db58d4dac3ecc90660593735193fe5c88
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-02-21 16:19:40 +0100

    quest2 ex5 push 7

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,9 +1,29 @@
+// Too many allocations ??
+// pub fn initials(names: Vec<&str>) -> Vec<String> {
+//     names
+//         .into_iter()
+//         .map(|name| name.chars().filter(|c| c.is_uppercase()))
+//         .map(|is| is.map(|initial| format!("{initial}.")))
+//         .map(|is| is.collect::<Vec<String>>().join(" "))
+//         .collect()
+// }
+
 pub fn initials(names: Vec<&str>) -> Vec<String> {
     names
         .into_iter()
-        .map(|name| name.chars().filter(|c| c.is_uppercase()))
-        .map(|is| is.map(|initial| format!("{initial}.")))
-        .map(|is| is.collect::<Vec<String>>().join(" "))
+        .map(|name| {
+            name.chars().filter(|c| c.is_uppercase()).fold(
+                String::with_capacity(5),
+                |mut acc, c| {
+                    if !acc.is_empty() {
+                        acc.push(' ');
+                    }
+                    acc.push(c);
+                    acc.push('.');
+                    acc
+                },
+            )
+        })
         .collect()
 }
 
