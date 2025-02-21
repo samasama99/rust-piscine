a907993a765aebf77997dc90fa8a709335dfba48
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-02-21 11:35:13 +0100

    ex9 push 1

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Matrix((i32, i32), (i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    let Matrix((a, b), (c, d)) = m;
+    Matrix((a, c), (b, d))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(transpose(Matrix((1, 3), (4, 5))), Matrix((1, 4), (3, 5)));
+    }
+}
