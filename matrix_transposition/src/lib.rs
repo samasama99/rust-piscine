a71c60dae176c2ecd80c20d0e1e802022e67d443
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-02-21 11:36:08 +0100

    ex9 push 2

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Matrix((i32, i32), (i32, i32));
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
     let Matrix((a, b), (c, d)) = m;
