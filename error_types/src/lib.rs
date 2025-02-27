05658fe7b4c204568a08b76305eeb8ff03854f42
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-02-27 11:15:48 +0000

    push

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -37,9 +37,9 @@ pub use chrono::{NaiveDate, Utc};
 // this will be the structure that wil handle the errors
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
-    form_values: (String, String),
-    date: String,
-    err: String,
+    pub form_values: (String, String),
+    pub date: String,
+    pub err: String,
 }
 impl FormError {
     pub fn new(field_name: String, field_value: String, err: String) -> FormError {
@@ -53,11 +53,11 @@ impl FormError {
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Form {
-    first_name: String,
-    last_name: String,
-    birth: NaiveDate,
-    birth_location: String,
-    password: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub birth: NaiveDate,
+    pub birth_location: String,
+    pub password: String,
 }
 
 impl Form {
