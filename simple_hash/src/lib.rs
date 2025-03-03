d44d2d2287fd25c4da417774e8e69369177effe9
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-03-03 11:49:53 +0000

    push

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -13,7 +13,7 @@
 
 use std::collections::{HashMap, HashSet};
 
-pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
+pub fn word_frequency_counter<'a>(words: &'a Vec<&'a str>) -> HashMap<&'a str, usize> {
     words.into_iter().fold(HashMap::new(), |mut acc, word| {
         acc.entry(word)
             .and_modify(|count| {
@@ -45,7 +45,7 @@ mod tests {
     fn it_works() {
         const SENTENCE: &str = "this is a very basic sentence with only a few repetitions. once again this is very basic but it should be enough for basic tests";
         let words = SENTENCE.split_ascii_whitespace().collect::<Vec<_>>();
-        let frequency_count = word_frequency_counter(words);
+        let frequency_count = word_frequency_counter(&words);
 
         assert_eq!(
             HashMap::from([
