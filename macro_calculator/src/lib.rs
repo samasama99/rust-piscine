bd1fce515bf0d1e74d5b0816b8af93871f08212e
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-03-05 09:58:15 +0000

    push

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -9,32 +9,28 @@ pub struct Food {
     pub nbr_of_portions: f64,
 }
 
-fn convert_to_kcal(calories: Vec<String>) -> Vec<f64> {
+fn sum_cals(calories: Vec<String>) -> f64 {
     let mut kcal_values: Vec<f64> = Vec::new();
 
     for cal in calories {
-        if cal.contains("kJ") {
-            let value: f64 = cal.trim_end_matches("kJ").parse().unwrap();
-            let kcal = value / 4.18;
-            kcal_values.push(kcal);
-        } else if cal.contains("kcal") {
+        if cal.contains("kcal") {
             let value: f64 = cal.trim_end_matches("kcal").parse().unwrap();
-            kcal_values.push(0f64);
+            kcal_values.push(value);
         }
     }
 
-    kcal_values
+    kcal_values.iter().sum::<f64>()
 }
 
 pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
     let res = foods.into_iter().fold(object! {
-                "cals": 0,
-                "carbs": 0,
-                "proteins": 0,
-                "fats": 0
-    },|acc, food| {
-       object! {
-           "cals" : acc["cals"].as_f64().unwrap() + convert_to_kcal(food.calories.to_vec()).iter().sum::<f64>() * food.nbr_of_portions,
+                "cals": 0.0,
+                "carbs": 0.0,
+                "proteins": 0.0,
+                "fats": 0.0
+    }, |acc, food| {
+        object! {
+           "cals" : acc["cals"].as_f64().unwrap() + sum_cals(food.calories.to_vec()) * food.nbr_of_portions,
            "carbs" : acc["carbs"].as_f64().unwrap() + food.carbs * food.nbr_of_portions,
            "proteins" : acc["proteins"].as_f64().unwrap() + food.proteins * food.nbr_of_portions,
            "fats" : acc["fats"].as_f64().unwrap() + food.fats * food.nbr_of_portions,
@@ -75,22 +71,10 @@ mod tests {
         ];
 
         let res = calculate_macros(a);
-      assert_eq!(
-         res["cals"].as_f32().unwrap(),
-          2777.39
-      );
+        assert_eq!(res["cals"].as_f32().unwrap(), 2777.39);
 
-        assert_eq!(
-            res["carbs"].as_f32().unwrap(),
-            322.44
-        );
-        assert_eq!(
-            res["proteins"].as_f32().unwrap(),
-            122.06
-        );
-        assert_eq!(
-             res["fats"].as_f32().unwrap(),
-            106.93
-        );
+        assert_eq!(res["carbs"].as_f32().unwrap(), 322.44);
+        assert_eq!(res["proteins"].as_f32().unwrap(), 122.06);
+        assert_eq!(res["fats"].as_f32().unwrap(), 106.93);
     }
 }
