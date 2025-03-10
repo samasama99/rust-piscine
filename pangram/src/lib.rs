29f818d0a1c00a2d04ae7a52b2b970670a849755
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-03-10 10:40:23 +0000

    push

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -5,13 +5,14 @@
 use std::collections::HashSet;
 
 pub fn is_pangram(s: &str) -> bool {
-    s.chars().all(|c| c.is_ascii())
-        && s.chars()
-            .filter(|c| c.is_alphabetic())
-            .map(|c| c.to_ascii_lowercase())
-            .collect::<HashSet<_>>()
-            .len()
-            == 26
+    //s.chars().all(|c| c.is_ascii())
+    //&&
+    s.chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<HashSet<_>>()
+        .len()
+        == 26
 }
 
 #[cfg(test)]
@@ -25,7 +26,7 @@ mod tests {
         assert!(is_pangram("the quick brown fox jumps over the lazy dog!"));
         assert!(is_pangram("this is not a pangram!").not());
         assert!(
-            is_pangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.").not()
+            is_pangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.")
         );
     }
 }
