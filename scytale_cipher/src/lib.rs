c2b5cbde8803ee9226c0cc9dd047caeaf730c47a
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-03-11 14:00:02 +0000

    push

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,55 @@
+use std::ops::Not;
+
+fn scytale_cipher(mut message: String, i: u32) -> String {
+    let mut chunks: Vec<Vec<_>> = Vec::new();
+    for c in message.chars() {
+        if let Some(last) = chunks.last_mut() {
+            if last.len() < i as usize {
+                last.push(c);
+            } else {
+                chunks.push(vec![c]);
+            }
+        } else {
+            chunks.push(vec![c]);
+        }
+    }
+
+    if let Some(last) = chunks.last_mut() {
+        if last.len() < i as usize {
+            last.extend(vec![' '; i as usize - last.len()]);
+            message.extend(vec![' '; i as usize - last.len()]);
+        }
+    }
+
+
+    let mut not_empty = true;
+    let mut res = vec![];
+    while not_empty {
+        not_empty = false;
+        for chunk in &mut chunks {
+            if chunk.is_empty().not() {
+                not_empty = true;
+                res.push(chunk.remove(0));
+            }
+        }
+    }
+
+    res.iter().collect::<String>().trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::scytale_cipher;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            "sec yCtoadle",
+            scytale_cipher(String::from("scytale Code"), 6)
+        );
+        assert_eq!(
+            "sCcoydtea l e",
+            scytale_cipher(String::from("scytale Code"), 8)
+        );
+    }
+}
