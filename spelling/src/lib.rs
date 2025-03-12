3ac9f31787e9cb3939ebe30748a717ed4c988f1a
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-03-12 13:33:03 +0000

    push

---
+++ spelling/src/lib.rs
@@ -0,0 +1,64 @@
+pub fn spell(n: u64) -> String {
+    match n {
+        0 => "zero".to_string(),
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        4 => "four".to_string(),
+        5 => "five".to_string(),
+        6 => "six".to_string(),
+        7 => "seven".to_string(),
+        8 => "eight".to_string(),
+        9 => "nine".to_string(),
+        10 => "ten".to_string(),
+        11 => "eleven".to_string(),
+        12 => "twelve".to_string(),
+        13 => "thirteen".to_string(),
+        14 => "fourteen".to_string(),
+        15 => "fifteen".to_string(),
+        16..=17 => format!("{}teen", spell(n % 10)),
+        18 => "eighteen".to_string(),
+        19 => format!("{}-teen", spell(n % 10)),
+        20 => "twenty".to_string(),
+        20..=29 => format!("twenty-{}", spell(n % 10)),
+        30 => "thirty".to_string(),
+        30..=39 => format!("thirty-{}", spell(n % 10)),
+        41 => "forty".to_string(),
+        40..=49 => format!("forty-{}", spell(n % 10)),
+        51 => "fifty".to_string(),
+        50..=59 => format!("fifty-{}", spell(n % 10)),
+        61 => "sixty".to_string(),
+        60..=69 => format!("sixty-{}", spell(n % 10)),
+        71 => "seventy".to_string(),
+        70..=79 => format!("seventy-{}", spell(n % 10)),
+        81 => "seventy".to_string(),
+        80..=89 => format!("eighty-{}", spell(n % 10)),
+        91 => "ninety".to_string(),
+        90..=99 => format!("ninety-{}", spell(n % 10)),
+        100 => "one hundred".to_string(),
+        101..=999 => format!("{} hundred {}", spell(n / 100), spell(n % 100)).to_string(),
+        1000 => "one thousand".to_string(),
+        1001..=999999 => format!(
+            "{} thousand {} hundred {}",
+            spell(n / 1000),
+            spell(n / 100 % 10),
+            spell(n % 100)
+        )
+        .to_string(),
+        1_000_000 => "one million".to_string(),
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!("zero", spell(0));
+        assert_eq!("eleven", spell(11));
+        assert_eq!("three hundred forty-eight", spell(348));
+        assert_eq!("nine thousand nine hundred ninety-six", spell(9996));
+    }
+}
