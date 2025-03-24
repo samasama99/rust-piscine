593173486f3c097b983ec20159b4a76383675acf
oussama rahmouni <55030945+samasama99@users.noreply.github.com>
2025-03-24 15:45:59 +0000

    push

--- spelling/src/lib.rs
+++ spelling/src/lib.rs
@@ -20,21 +20,21 @@ pub fn spell(n: u64) -> String {
         18 => "eighteen".to_string(),
         19 => format!("{}-teen", spell(n % 10)),
         20 => "twenty".to_string(),
-        20..=29 => format!("twenty-{}", spell(n % 10)),
+        21..=29 => format!("twenty-{}", spell(n % 10)),
         30 => "thirty".to_string(),
         30..=39 => format!("thirty-{}", spell(n % 10)),
-        41 => "forty".to_string(),
-        40..=49 => format!("forty-{}", spell(n % 10)),
-        51 => "fifty".to_string(),
-        50..=59 => format!("fifty-{}", spell(n % 10)),
-        61 => "sixty".to_string(),
-        60..=69 => format!("sixty-{}", spell(n % 10)),
-        71 => "seventy".to_string(),
-        70..=79 => format!("seventy-{}", spell(n % 10)),
-        81 => "seventy".to_string(),
-        80..=89 => format!("eighty-{}", spell(n % 10)),
-        91 => "ninety".to_string(),
-        90..=99 => format!("ninety-{}", spell(n % 10)),
+        40 => "forty".to_string(),
+        41..=49 => format!("forty-{}", spell(n % 10)),
+        50 => "fifty".to_string(),
+        51..=59 => format!("fifty-{}", spell(n % 10)),
+        60 => "sixty".to_string(),
+        61..=69 => format!("sixty-{}", spell(n % 10)),
+        70 => "seventy".to_string(),
+        71..=79 => format!("seventy-{}", spell(n % 10)),
+        80 => "seventy".to_string(),
+        81..=89 => format!("eighty-{}", spell(n % 10)),
+        90 => "ninety".to_string(),
+        91..=99 => format!("ninety-{}", spell(n % 10)),
         100 => "one hundred".to_string(),
         101..=999 => format!("{} hundred {}", spell(n / 100), spell(n % 100)).to_string(),
         1000 => "one thousand".to_string(),
@@ -67,5 +67,6 @@ mod tests {
         assert_eq!("three hundred forty-eight", spell(348));
         assert_eq!("one thousand fifty-five", spell(1055));
         assert_eq!("nine thousand nine hundred ninety-six", spell(9996));
+        assert_eq!("six hundred fifty thousand one hundred twenty-three", spell(650_123));
     }
 }
